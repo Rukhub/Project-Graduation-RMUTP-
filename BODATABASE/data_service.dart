7cd14b7abb65626a965aecf14180f55cda884a6a
Sutthamet Sutthikun <sutthamet.4869@gmail.com>
2026-01-15 20:09:32 +0700

    Update data_service.dart

--- BODATABASE/data_service.dart
+++ BODATABASE/data_service.dart
@@ -1,185 +1,85 @@
-import 'package:flutter/material.dart';
-import 'menu.dart'; 
-import 'data_service.dart'; // <-- ต้องมั่นใจว่ารักมีไฟล์นี้และ import มาแล้ว
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginPage(),
-    );
-  }
-}
-
-class LoginPage extends StatefulWidget { // เปลี่ยนเป็น StatefulWidget เพื่อจัดการค่าที่พิมพ์
-  const LoginPage({super.key});
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  // 1. เพิ่มตัวรับข้อมูลจากช่องพิมพ์
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFF9A2C2C),
-      body: Column(
-        children: [
-          const SizedBox(height: 100),
-          Image.network(
-            'https://eng.rmutp.ac.th/web2558/wp-content/uploads/2024/03/%E0%B9%80%E0%B8%9F%E0%B8%B7%E0%B8%AD%E0%B8%87%E0%B9%81%E0%B8%94%E0%B8%872-350x350.png',
-            height: 200,
-          ),
-          const SizedBox(height: 30),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(40),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.vertical(top: Radius.circular(50)),
-              ),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    const Text(
-                      'ระบบจัดเก็บข้อมูลครุภัณฑ์',
-                      style: TextStyle(color: Color(0xFF9A2C2C), fontSize: 25, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 40),
-                    // 2. ใส่ Controller ในช่อง Username
-                    TextField(
-                      controller: _usernameController,
-                      decoration: InputDecoration(
-                        prefixIcon: const Icon(Icons.person),
-                        hintText: 'Username',
-                        filled: true,
-                        fillColor: Colors.white,
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: BorderSide(color: Colors.grey.shade400, width: 2),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    // 3. ใส่ Controller ในช่อง Password
-                    TextField(
-                      controller: _passwordController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        prefixIcon: const Icon(Icons.lock),
-                        hintText: 'Password',
-                        filled: true,
-                        fillColor: Colors.white,
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: BorderSide(color: Colors.grey.shade400, width: 2),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 50),
-                    SizedBox(
-                      width: double.infinity,
-                      height: 60,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xFF9A2C2C),
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                        ),
-                        onPressed: () async {
-                          // --- 🚀 ส่วนสำคัญ: ส่งข้อมูลไปเช็คที่เครื่องโบ ---
-                          final result = await DataService().login(
-                            _usernameController.text,
-                            _passwordController.text,
-                          );
-
-                          if (result != null) {
-                            // ถ้า Login สำเร็จ ให้ไปหน้า Menu
-                            if (!mounted) return;
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => const MenuScreen()),
-                            );
-                          } else {
-                            // ถ้า Login ไม่สำเร็จ ให้ขึ้นเตือน
-                            if (!mounted) return;
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(content: Text('Username หรือ Password ไม่ถูกต้อง!')),
-                            );
-                          }
-                        },
-                        child: const Text(
-                          'Login',
-                          style: TextStyle(fontSize: 24, color: Colors.white, fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ),
-const SizedBox(height: 15),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Divider(
-                            color: Colors.grey.shade400,
-                            thickness: 1.2,
-                          ),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 12),
-                          child: Text(
-                            'or',
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Divider(
-                            color: Colors.grey.shade400,
-                            thickness: 1.2,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 40),
-                    Column(
-                      children: [
-                        CircleAvatar(
-                          radius: 28,
-                          backgroundColor: Colors.white,
-                          child: ClipOval(
-                            child: Image.network(
-                              'https://cdn-icons-png.flaticon.com/512/2991/2991148.png', // เปลี่ยนเป็นรูป Google จริงๆ
-                              width: 56,
-                              height: 56,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        const Text(
-                          'Login with your others account',
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+[{
+	"resource": "/C:/Users/Sutthamet/Documents/flutter_projects/ticket_app/lib/data_service.dart",
+	"owner": "_generated_diagnostic_collection_name_#0",
+	"code": {
+		"value": "undefined_method",
+		"target": {
+			"$mid": 1,
+			"path": "/diagnostics/undefined_method",
+			"scheme": "https",
+			"authority": "dart.dev"
+		}
+	},
+	"severity": 8,
+	"message": "The method 'DataService' isn't defined for the type '_LoginPageState'.\nTry correcting the name to the name of an existing method, or defining a method named 'DataService'.",
+	"source": "dart",
+	"startLineNumber": 101,
+	"startColumn": 48,
+	"endLineNumber": 101,
+	"endColumn": 59,
+	"modelVersionId": 3,
+	"origin": "extHost1"
+},{
+	"resource": "/C:/Users/Sutthamet/Documents/flutter_projects/ticket_app/lib/data_service.dart",
+	"owner": "_generated_diagnostic_collection_name_#0",
+	"code": {
+		"value": "unused_import",
+		"target": {
+			"$mid": 1,
+			"path": "/diagnostics/unused_import",
+			"scheme": "https",
+			"authority": "dart.dev"
+		}
+	},
+	"severity": 4,
+	"message": "Unused import: 'data_service.dart'.\nTry removing the import directive.",
+	"source": "dart",
+	"startLineNumber": 3,
+	"startColumn": 8,
+	"endLineNumber": 3,
+	"endColumn": 27,
+	"modelVersionId": 3,
+	"origin": "extHost1"
+},{
+	"resource": "/C:/Users/Sutthamet/Documents/flutter_projects/ticket_app/lib/data_service.dart",
+	"owner": "_generated_diagnostic_collection_name_#0",
+	"code": {
+		"value": "use_build_context_synchronously",
+		"target": {
+			"$mid": 1,
+			"path": "/diagnostics/use_build_context_synchronously",
+			"scheme": "https",
+			"authority": "dart.dev"
+		}
+	},
+	"severity": 2,
+	"message": "Don't use 'BuildContext's across async gaps, guarded by an unrelated 'mounted' check.\nGuard a 'State.context' use with a 'mounted' check on the State, and other BuildContext use with a 'mounted' check on the BuildContext.",
+	"source": "dart",
+	"startLineNumber": 110,
+	"startColumn": 31,
+	"endLineNumber": 110,
+	"endColumn": 38,
+	"modelVersionId": 3,
+	"origin": "extHost1"
+},{
+	"resource": "/C:/Users/Sutthamet/Documents/flutter_projects/ticket_app/lib/data_service.dart",
+	"owner": "_generated_diagnostic_collection_name_#0",
+	"code": {
+		"value": "use_build_context_synchronously",
+		"target": {
+			"$mid": 1,
+			"path": "/diagnostics/use_build_context_synchronously",
+			"scheme": "https",
+			"authority": "dart.dev"
+		}
+	},
+	"severity": 2,
+	"message": "Don't use 'BuildContext's across async gaps, guarded by an unrelated 'mounted' check.\nGuard a 'State.context' use with a 'mounted' check on the State, and other BuildContext use with a 'mounted' check on the BuildContext.",
+	"source": "dart",
+	"startLineNumber": 116,
+	"startColumn": 50,
+	"endLineNumber": 116,
+	"endColumn": 57,
+	"modelVersionId": 3,
+	"origin": "extHost1"
+}]
